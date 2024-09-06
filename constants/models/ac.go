4b0920f01cec4fbb1cdd1a5527a281bfb6faf16d
RidhoAnjar <146798598+RidhoAnjarM@users.noreply.github.com>
2024-09-06 15:01:25 +0700

    Delete constants/models directory

--- constants/models/ac.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type AC struct {
-	ID    int     `gorm:"primaryKey" json:"id"`
-	Name  string  `json:"name"`
-	Brand string  `json:"brand"`
-	Pk    string  `json:"pk"`
-	Price float32 `json:"price"`
-}
--- constants/models/role.go
+++
@@ -1,7 +0,0 @@
-package models
-
-
-type Role struct {
-	ID   int    `gorm:"primaryKey" json:"id"`
-	Name string `gorm:"type:varchar(100)" json:"name"`
-}
